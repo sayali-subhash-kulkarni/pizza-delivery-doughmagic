af5d396936c3d4d9696addd9e31b7f2b264c64d5
sayali-subhash-kulkarni <38128081+sayali-subhash-kulkarni@users.noreply.github.com>
2018-04-08 01:03:09 +0530

    Add files via upload

---
+++ pizza.js
@@ -0,0 +1,36 @@
+{
+	"pizza": [
+	{ 
+		"p_name": "margarita pizza",
+		"ingredients":"margherita cheese,tomatoes topping",
+		"price":"RS 120"
+	},
+
+	{
+		"p_name": "Pepper Blast pizza",
+		"ingredients":"Red bell pepper, yellow bell pepper, capsicum, red paprika, jalapeno and exotic herb",
+		"price":"Rs 180"
+		
+
+	},
+
+	{	"p_name": "Paneer vaganza pizza",
+		"ingredients":"Desi paneer,spicy pepper,crisp capsicum",
+		"price":"Rs 200"
+		
+
+	}
+
+]
+
+}
+
+
+
+
+
+
+
+
+
+
